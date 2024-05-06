ccefee44582b954b70dae920d4f8c8da1069ebd1
erwinmareto <100197413+erwinmareto@users.noreply.github.com>
2024-05-06 23:16:16 +0700

    feat: installed GSAP and applied marquee animation (#2)
    * feat: added animations, changed folders, and added aspect ratios
    
    * feat: added responsiveness to marquee text and moved the bg in hero
    
    * feat: installed GSAP and migrated marquee animation to GSAP

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,8 @@
 import navLogo from "./attLogo.jpg";
 import mainShirt from "./mainShirt.jpg";
+import shirt1 from "./shirt1.jpeg";
+import shirt2 from "./shirt2.jpg";
+import shirt3 from "./shirt3.jpg";
+import shirt4 from "./shirt4.jpeg";
 
-export { navLogo, mainShirt };
+export { navLogo, mainShirt, shirt1, shirt2, shirt3, shirt4 };
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,7 +1,105 @@
-import { mainShirt } from "../assets/images";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { mainShirt, shirt1, shirt2, shirt3, shirt4 } from "../assets/images";
+
 import Button from "../parts/Button";
 
 const Showcase = () => {
+  const imgRef = useRef(null);
+
+  useGSAP(
+    () => {
+      const imgEl = imgRef.current;
+
+      gsap.fromTo(
+        ".whitespace-nowrap",
+        { x: "-40%" },
+        {
+          x: "0%",
+          ease: "none",
+          duration: 3,
+          repeat: -1,
+          yoyo: true,
+          // modifiers: {
+          //   x: gsap.utils.unitize(gsap.utils.wrap(0, -width)),
+          // },
+        }
+      );
+
+      gsap.fromTo(
+        imgEl,
+        { x: "25%" },
+        {
+          x: "-25%",
+          yoyo: true,
+          ease: "none",
+          duration: 3,
+          repeat: -1,
+          // modifiers: {
+          //   x: gsap.utils.unitize(gsap.utils.wrap(0, -width)),
+          // },
+        }
+      );
+      // gsap.fromTo(
+      //   ".group1",
+      //   { x: width },
+      //   {
+      //     x: -width,
+      //     ease: "none",
+      //     duration: 7,
+      //     repeat: -1,
+      //     // repeatDelay: 1,
+      //     // modifiers: {
+      //     //   x: gsap.utils.unitize(gsap.utils.wrap(0, -width)),
+      //     // },
+      //   }
+      // );
+      // gsap.fromTo(
+      //   ".group2",
+      //   { x: width },
+      //   {
+      //     x: -width - 200,
+      //     ease: "none",
+      //     duration: 7,
+      //     repeat: -1,
+      //     delay: 1,
+      //     // modifiers: {
+      //     //   x: gsap.utils.unitize(gsap.utils.wrap(0, -width)),
+      //     // },
+      //   }
+      // );
+      // gsap.fromTo(
+      //   ".group3",
+      //   { x: width },
+      //   {
+      //     x: -width - 300,
+      //     ease: "none",
+      //     duration: 7,
+      //     repeat: -1,
+      //     delay: 2,
+      //     // modifiers: {
+      //     //   x: gsap.utils.unitize(gsap.utils.wrap(0, -width)),
+      //     // },
+      //   }
+      // );
+      // gsap.fromTo(
+      //   ".group4",
+      //   { x: width },
+      //   {
+      //     x: -width - 400,
+      //     ease: "none",
+      //     duration: 8,
+      //     repeat: -1,
+      //     delay: 2,
+      //     // modifiers: {
+      //     //   x: gsap.utils.unitize(gsap.utils.wrap(0, -width)),
+      //     // },
+      //   }
+      // );
+    },
+    { scope: "#showcase" }
+  );
   return (
     <section id="showcase" className="max-container py-10 lg:py-16 xl:py-20">
       <div className="flex justify-center items-center mb-32">
@@ -14,48 +112,110 @@ const Showcase = () => {
         </h1>
       </div>
 
-      <div className="animate-marquee2 whitespace-nowrap flex gap-4">
-        <h1 className="text-4xl lg:text-8xl font-zing">ATTENTION!</h1>
-        <h1 className="text-4xl lg:text-8xl font-zing">ATTENTION!</h1>
-        <h1 className="text-4xl lg:text-8xl font-zing">ATTENTION!</h1>
-        <h1 className="text-4xl lg:text-8xl font-zing">ATTENTION!</h1>
-        <h1 className="text-4xl lg:text-8xl font-zing">ATTENTION!</h1>
+      <div className=" whitespace-nowrap flex gap-4">
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
       </div>
 
-      <div className="relative flex justify-center items-center gap-1 my-10">
+      <div
+        ref={imgRef}
+        className="relative flex justify-center items-center gap-1 my-10"
+      >
         <img
           src={mainShirt}
           alt="shirt"
-          className="aspect-[3/4] w-[200px] lg:w-[400px] animate-marquee"
+          className="images aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
         />
         <img
-          src={mainShirt}
+          src={shirt1}
           alt="shirt"
-          className="aspect-[3/4] w-[200px] lg:w-[400px] animate-marquee"
+          className="images aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
         />
         <img
-          src={mainShirt}
+          src={shirt2}
           alt="shirt"
-          className="aspect-[3/4] w-[200px] lg:w-[400px] animate-marquee"
+          className="images aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
         />
         <img
-          src={mainShirt}
+          src={shirt3}
           alt="shirt"
-          className="aspect-[3/4] w-[200px] lg:w-[400px] animate-marquee"
+          className="images aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
         />
         <img
-          src={mainShirt}
+          src={shirt4}
           alt="shirt"
-          className="aspect-[3/4] w-[200px] lg:w-[400px] animate-marquee"
+          className="images aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
         />
+        {/* <img
+          src={shirt3}
+          alt="shirt"
+          className="images aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
+        /> */}
       </div>
+      {/* <div
+        // ref={imgRef}
+        className="relative flex justify-center items-center my-10 bg-red-200"
+      >
+        <div className="flex gap-1 group1">
+          <img
+            src={mainShirt}
+            alt="shirt"
+            className="aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
+          />
+          <img
+            src={shirt1}
+            alt="shirt"
+            className="aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
+          />
+        </div>
+        <div className="flex gap-1 group2">
+          <img
+            src={shirt2}
+            alt="shirt"
+            className="aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
+          />
+          <img
+            src={shirt3}
+            alt="shirt"
+            className="aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
+          />
+        </div>
+        <div className="flex gap-1 group3">
+          <img
+            src={shirt4}
+            alt="shirt"
+            className="aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
+          />
+          <img
+            src={shirt3}
+            alt="shirt"
+            className="aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
+          />
+        </div>
+        <div className="flex gap-1 group4">
+          <img
+            src={shirt4}
+            alt="shirt"
+            className="aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
+          />
+          <img
+            src={shirt3}
+            alt="shirt"
+            className="aspect-[3/4] w-[200px] md:w-[300px] lg:w-[400px]"
+          />
+        </div>
+      </div> */}
 
-      <div className="animate-marquee2 whitespace-nowrap flex gap-4">
-        <h1 className="text-4xl lg:text-8xl font-zing">ATTENTION!</h1>
-        <h1 className="text-4xl lg:text-8xl font-zing">ATTENTION!</h1>
-        <h1 className="text-4xl lg:text-8xl font-zing">ATTENTION!</h1>
-        <h1 className="text-4xl lg:text-8xl font-zing">ATTENTION!</h1>
-        <h1 className="text-4xl lg:text-8xl font-zing">ATTENTION!</h1>
+      <div className=" whitespace-nowrap flex gap-4">
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
+        <h1 className="text-4xl md:text-8xl font-zing">ATTENTION!</h1>
       </div>
 
       <div className="flex flex-col justify-center items-center gap-16 my-32">
