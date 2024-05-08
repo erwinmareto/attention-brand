1b24ff35a2350798fc65ed5586e243855621e67f
erwinmareto <100197413+erwinmareto@users.noreply.github.com>
2024-05-08 20:20:06 +0700

    feat: finalized footer and and changed navbar links (#5)
    * chore: changed the title and favicon
    
    * feat: finalized footer and and changed navbar links

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,10 +14,9 @@ import { FaMoneyBillTrendUp } from "react-icons/fa6";
 import { FaRegSun } from "react-icons/fa";
 
 export const navLinks = [
-  { href: "#home", label: "Home" },
   { href: "#about-us", label: "About Us" },
   { href: "#products", label: "Products" },
-  { href: "#contact-us", label: "Contact Us" },
+  { href: "#benefits", label: "Benefits" },
 ];
 
 export const productsList = [
