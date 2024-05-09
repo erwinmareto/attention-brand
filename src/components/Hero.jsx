d047656acd5196e8823eb1f358ab862576f4185a
erwinmareto <100197413+erwinmareto@users.noreply.github.com>
2024-05-10 00:08:38 +0700

    feat: revamped the hero and tweaked the mobile menu (#8)
    * feat: redesigned hero
    
    * feat: changed the font sizes for responsiveness
    
    * feat: changed the background and added cancel button to mobile menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { scenery } from "../assets/images";
 import Button from "../parts/Button";
 
 const Hero = () => {
@@ -9,11 +8,6 @@ const Hero = () => {
 
   useGSAP(
     () => {
-      gsap.fromTo(
-        ".hero-img",
-        { scaleX: 0 },
-        { scaleX: 1, transformOrigin: "top" }
-      );
       gsap.fromTo(
         ".letter",
         { opacity: 0 },
@@ -23,12 +17,10 @@ const Hero = () => {
     { scope: heroRef }
   );
   return (
-    <section ref={heroRef} className="max-container relative z-10">
-      <div className="w-full min-h-screen lg:flex bg-hero bg-left">
-        <div className="xl:w-3/5 flex flex-col max-lg:items-center justify-center px-4 py-28 md:padding-x max-lg:text-center">
-          <h1 className="text-8xl font-montserrat max-sm:text-[72px] max-sm:leading-[82px]">
-            Your
-            <br />
+    <section ref={heroRef} className="max-container">
+      <div className="w-full min-h-screen flex justify-center items-center bg-hero bg-center">
+        <div className="flex flex-col justify-center items-center text-center w-full gap-4">
+          <h1 className="text-[5rem] md:text-[8rem] lg:text-[10rem] font-zing">
             <span className="letter font-zing transition-colors hover:text-dark-red">
               A
             </span>
@@ -59,28 +51,15 @@ const Hero = () => {
             <span className="letter font-zing transition-colors hover:text-dark-red">
               !
             </span>
-            <br />
-            Please
           </h1>
-          <p className="font-montserrat max-w-sm text-primary text-lg leading-7 mt-6 max-sm:text-[1rem]">
-            Brand kaos dengan desain yang{" "}
+          <p className="font-montserrat text-sm text-primary max-w-xl mt-10 md:text-xl">
+            Merek kaos dengan desain yang{" "}
             <span className="text-dark-red font-bold italic">nyeleneh</span> dan
             memiliki harga yang terjangkau
           </p>
-          <div className="mt-10">
-            <Button href="#products" samePage>
-              Our Products
-            </Button>
-          </div>
-        </div>
-        <div className="bg-red-300 xl:w-2/5">
-          <img
-            src={scenery}
-            alt="shirt"
-            // width={610}
-            // height={500}
-            className="object-cover hero-img"
-          />
+          <Button href="/#products" samePage>
+            Our Products
+          </Button>
         </div>
       </div>
     </section>
--- src/components/Navbar/MobileMenu.jsx
+++ src/components/Navbar/MobileMenu.jsx
@@ -2,7 +2,23 @@ import { navLinks } from "../../constants";
 
 const MobileMenu = ({ isOpen, setIsOpen }) => {
   return (
-    <nav className="fixed w-full h-screen flex flex-col justify-center bg-hero bg-cover bg-left z-50 lg:hidden">
+    <nav className="fixed w-full h-screen flex flex-col justify-center bg-nav bg-cover bg-left z-50 lg:hidden">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        onClick={() => setIsOpen(!isOpen)}
+        className="w-14 h-14 fixed top-10 right-10 text-8xl"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6 18 18 6M6 6l12 12"
+        />
+      </svg>
+
       <ul className="flex flex-col items-center text-4xl text-primary gap-16 font-montserrat">
         {navLinks.map((link) => (
           <li key={link.href}>
